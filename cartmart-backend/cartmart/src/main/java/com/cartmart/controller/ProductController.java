fffe8fc0c1cf501a547b245803b73e6ba661823f
Gajanan <105433300+ErGajananKumbhar@users.noreply.github.com>
2025-04-08 14:05:26 +0530

    Update ProductController.java

--- cartmart-backend/cartmart/src/main/java/com/cartmart/controller/ProductController.java
+++ cartmart-backend/cartmart/src/main/java/com/cartmart/controller/ProductController.java
@@ -17,7 +17,6 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
-        System.out.println("product getting");
     }
 
     @GetMapping("/{id}")
